7b2c7522b3a43cbdb1033350610cdbd9e09bd4aa
moeka kitamura <moeka.k76@gmail.com>
2021-08-13 08:55:16 +0900

    hide edit and delete unless current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,11 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      redirect_to new_session_path, alert: 'ログインに失敗しました'
+      redirect_to new_session_path, alert: '⚠️Not a valid account name or password.'
     end
   end
   def destroy
     session.delete(:user_id)
-    redirect_to new_session_path, notice: 'ログアウトしました'
+    redirect_to new_session_path, notice: 'You have been successfully logged out.'
   end
 end
