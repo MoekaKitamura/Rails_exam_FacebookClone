352a22f0c6c8ac7e87b560c051976b650ecbefde
moeka kitamura <moeka.k76@gmail.com>
2021-08-12 09:53:30 +0900

    migrate, routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :pictures
+  resources :users, only: [:new, :create, :show] 
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
